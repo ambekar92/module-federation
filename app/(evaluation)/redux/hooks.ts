4a3c1d53c37ff38b25f27d2d4d0bb0143bcd23bf
Santhosh K <santhoshkcse4@gmail.com>
2024-10-04 08:14:36 +0530

    new 006

--- app/(evaluation)/redux/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, EvaluationDispatch } from './evaluationStore';
-
-export const useEvaluationDispatch = () => useDispatch<EvaluationDispatch>();
-export const useEvaluationSelector: TypedUseSelectorHook<RootState> = useSelector;
--- app/services/queries/application-service/useGetResponses.ts
+++
@@ -1,9 +0,0 @@
-import { QUESTIONNAIRE_ROUTE } from '@/app/constants/routes';
-import useSWR from 'swr';
-import fetcher from '../../fetcher';
-import { QuestionAnswer } from '@/app/shared/form-builder/questionnaire-types/question';
-
-export function useGetResponses(sectionUrl: string) {
-
-  return useSWR<QuestionAnswer[]>( sectionUrl ? `${QUESTIONNAIRE_ROUTE}/${sectionUrl}`: null)
-}
